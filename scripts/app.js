0cef70d13125b376d295f8649b86c4e504ed7251
Alejandro Echeverria <alejandro@echev.com>
2020-08-21 11:20:40 -0400

    feat(address balance): allows pasting new addresses and showing its balance

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,13 @@
 const client = new BlockchainApiCLient();
-load();
 
-async function load() {
-  const addressData = await client.getAddress('3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd');
+document.addEventListener('paste', async (event) => {
+  const address = (event.clipboardData || window.clipboardData).getData('text');
+  const addressesList = document.getElementById("addresses");
+  const addressElement = document.createElement("li");
+  addressesList.appendChild(addressElement);
+  addressElement.textContent = `${address}: loading...`;
+  const addressData = await client.getAddress(address);
+  addressElement.textContent = `${address}: ₿ ${addressData.final_balance / 10.0e7}`;
   console.log(addressData);
-}
+  event.preventDefault();
+});
--- scripts/blockchain_api_client.js
+++ scripts/blockchain_api_client.js
@@ -1,14 +1,14 @@
-const API_BASE_URL = 'https://blockchain.info/';
+const API_BASE_URL = 'https://api.blockcypher.com/v1/btc/main/';
 
 class BlockchainApiCLient {
   async getAddress(address) {
-    let response = await fetch(API_BASE_URL + 'rawaddr/' + address);
+    let response = await fetch(API_BASE_URL + 'addrs/' + address);
     let data = await response.json()
     return data;
   }
 
   async getTransaction(tx) {
-    let response = await fetch(API_BASE_URL + 'rawtx/' + tx);
+    let response = await fetch(API_BASE_URL + 'txs/' + tx);
     let data = await response.json()
     return data;
   }
