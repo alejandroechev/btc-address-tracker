0947ba9c3efec2603249ad316c1ab3d41b1ef0c5
Alejandro Echeverria <alejandro@echev.com>
2020-08-21 11:38:12 -0400

    feat(transactions): show tx data

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,20 @@ document.addEventListener('paste', async (event) => {
   const addressesList = document.getElementById("addresses");
   const addressElement = document.createElement("li");
   addressesList.appendChild(addressElement);
+  // addressElement.addEventListener('click' async (innerEvent) => {
+
+  // }));
   addressElement.textContent = `${address}: loading...`;
   const addressData = await client.getAddress(address);
   addressElement.textContent = `${address}: ₿ ${addressData.final_balance / 10.0e7}`;
   console.log(addressData);
+  const txList = document.getElementById("transactions");
+  addressData.txrefs.forEach(async (tx) => {
+    const txElement = document.createElement("li");
+    txList.appendChild(txElement);
+    txElement.textContent = `${tx.tx_hash}: loading`;
+    const txData = await client.getTransaction(tx.tx_hash);
+    txElement.textContent = `${tx.tx_hash}: ₿ ${txData.fees  / 10.0e7}`;
+  });
   event.preventDefault();
 });
